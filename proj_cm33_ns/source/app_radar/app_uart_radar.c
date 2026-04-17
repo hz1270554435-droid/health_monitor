8210572bc63c6f5892eb8f7a51a13c8b0e7d0026
HZ <1270554435@qq.com>
2026-04-17 18:51:21 +0800

    temp

--- proj_cm33_ns/source/app_radar/app_uart_radar.c
+++ proj_cm33_ns/source/app_radar/app_uart_radar.c
@@ -5,7 +5,6 @@ static enum {waiting_SOF ,
             state_ID ,
             state_LEN ,
             state_TYPE ,
-            state_SOF ,
             state_Head_CK ,
             state_DATA ,
             state_Data_CK}RECIEVE_FSM_STATE;
@@ -18,11 +17,11 @@ cy_rslt_t APP_UART_RADAR_Init(void)
 
 static uint32_t read_uart_data()
 {
-
+    
 }
 
 cy_rslt_t APP_UART_RADAR_GetData(uint32_t* Reciece_buffer)
 {
-    Reciece_buffer = read_uart_data();
+    *Reciece_buffer = read_uart_data();
     return CY_RSLT_SUCCESS;
 }
